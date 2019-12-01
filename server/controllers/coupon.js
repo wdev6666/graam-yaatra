71e4043d2951e96330450c3154d4b8f6a8735afd
Naresh <wdev6666@gmail.com>
2019-12-01 17:33:51 +0530

    Demo

--- server/controllers/coupon.js
+++ server/controllers/coupon.js
@@ -9,9 +9,7 @@ module.exports = {
       .then(coupons => {
         res.send(coupons);
       })
-      .catch(err =>
-        next({ statusCode: 404, message: 'No coupons found for the day!' })
-      );
+      .catch(err => next({ statusCode: 404, message: err }));
   },
 
   listbydate(req, res, next) {
